7a6abc9d8a1b1f0de240c1270a493d1c04961361
NosheenIqbal <buttnosheen874@gmail.com>
2024-08-24 14:35:31 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+//Chapter1
+//Question1
+//alert("Hello world");
+//question2
+//alert("Hello world 2";
+//question3
+//alert("Hello/nHello world 3");
+
+//chapter2
+//question1
+//var username;
+//question2
+//Chapter2
+//var myName="Nosheen";
+//question3
+//var message="Hello World";
+//question4
+//alert(message);
+//var studentName="Nosheen";
+//var studentAge="23";
+//alert(studentName);
+//alert(studentAge);
+//question6
+//var  Email="buttnosheen874@gmailcom";
+//alert(Email);
+//question 7
+//var book="A smarter way to learn JavaScript";
+//alert(book);
+//question 8
+//document.write("Yah! I can write HTML content through JavaScript");
+//question9
+//var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(design);
+
+//chapter3
+//question1
+//var age="I am 28 years old";
+//alert(age);
+//question2
+//var visit="you have visited this site 14 times";
+//alert(visit);
+//question3
+//var birthyear="my birth year is 1998";
+//alert(birthyear);
+//question4
+//var visitorName="my name is nosheen";
+//var productTitle="product title";
+//var quantity="quantity";
+//alert(visitorName);
+//alert(productTitle);
+//alert(quantity);
+
